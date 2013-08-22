05468194886ff6d250a03e376428110e4e496468
fakestarbaby <fakestarbaby@gmail.com>
2013-08-22 11:23:32 +0900

    Added demo cms

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleBrowsercms::Application.config.secret_token = 'f2a38a3b4ebec1f7035f936acb85be4887e54751078d41093f2e6e59db3b174e839b93e55fcb32b6ae3595bf1460822ca4243a945daeb5b307a67cce4cc7fbc0'
